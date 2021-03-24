882773021f359df04739ee0ed4859e18ce621ad0
Isaac Mendez <IsaacMendez.work@gmail.com>
2021-03-23 20:57:27 -0400

    Removed user stories and framework

--- group-weather-app/src/components/Home.js
+++ group-weather-app/src/components/Home.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import iFrame from './iFrame';
- 
+import Background from '../css/Background.css'
+import Footer from './Footer.js';
+
+function Home() {
   return (
-    <iFrame></iFrame>
+    <div className="background">
+      <h1>Home</h1>
       <div>
-        <h2 style={{textAlign: 'center'}}>User Stories</h2>
-        <h4>User One</h4>
-        <p>Max needs accurate information on the weather since he travels frequently for work.  He wants a weather app that he can use to pull up weather information quickly and easily.  This app will ensure that Max is also prepared regardless of the city he is working in.</p>
-        <h4>User Two</h4>
-        <p>As a pet-owner, I need to know the weather for my dog’s daily walks.  I need an app that is always accurate.  Having an accurate weather app will allow me to prep my dog’s weather appropriate clothing (doggy shoes for hot days, raincoat etc.)</p>
+        <div className="foot">
+          <Footer />
+        </div>
       </div>
-    </div>;
-  }
+    </div>
+  )
 }
 
 export default Home
