9bde5263ae361f7a3bd85712410d02362cc9cbe5
Isaac Mendez <IsaacMendez.work@gmail.com>
2021-03-23 20:55:13 -0400

    Updated to include visual displays

--- group-weather-app/src/components/daily.js
+++ group-weather-app/src/components/daily.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Day from '../css/Day.css'
+
 export default class Daily extends Component {
   constructor() {
     super();
@@ -9,40 +11,44 @@ export default class Daily extends Component {
       wind: ['4MPH', '11MPH', '17MPH', '10MPH', '7MPH']
     }
   }
+
   render() {
     return (
-      <div>
-        <div>
-        <h3>{this.state.location[0]}</h3>
+      <div className="container">
+        <div className="Text">
+          <div className="cold">
+        <h3 className="New">{this.state.location[0]}</h3>
           <p>Temp: {this.state.temp[0]}</p>
           <p>Precipitation: {this.state.precip[0]}</p>
           <p>Wind: {this.state.wind[0]}</p>
       </div>
-        <div>
-          <h3>{this.state.location[1]}</h3>
+      <div className="Rainy">
+        <h3>{this.state.location[1]}</h3>
           <p>Temp: {this.state.temp[1]}</p>
           <p>Precipitation: {this.state.precip[1]}</p>
           <p>Wind: {this.state.wind[1]}</p>
-        </div>
-        <div>
-          <h3>{this.state.location[2]}</h3>
+      </div>
+      <div className="Dry">
+        <h3>{this.state.location[2]}</h3>
           <p>Temp: {this.state.temp[2]}</p>
           <p>Precipitation: {this.state.precip[2]}</p>
           <p>Wind: {this.state.wind[2]}</p>
-        </div>
-        <div>
-          <h3>{this.state.location[3]}</h3>
+      </div>
+      <div>
+        <h3>{this.state.location[3]}</h3>
           <p>Temp: {this.state.temp[3]}</p>
           <p>Precipitation: {this.state.precip[3]}</p>
           <p>Wind: {this.state.wind[3]}</p>
-        </div>
-        <div>
+      </div>
+      <div className="Sunny">
           <h3>{this.state.location[4]}</h3>
           <p>Temp: {this.state.temp[4]}</p>
           <p>Precipitation: {this.state.precip[4]}</p>
           <p>Wind: {this.state.wind[4]}</p>
         </div>
       </div>
+      <div className="Day"></div>
+    </div>
     )
   }
 }
